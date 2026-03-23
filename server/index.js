5fbccbfbd5106849a5b1810206c835d667ef82e8
Jagrithanda <jagrithanda2011@gmail.com>
2026-03-23 14:57:11 +0530

    Todo backend file

---
+++ server/index.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+// const { Pool } = require("pg");
+const express = require("express");
+const cors = require("cors");
+const { todoRouter } = require('./routes/todo.js')
+
+const app = express();
+const {query} = require('./helpers/db.js')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+
+app.use('/', todoRouter)
+
+const port = process.env.PORT || 3001
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
+
+
---
+++ server/routes/todo.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const { query } = require('../helpers/db.js') // Note the double dots to go up a folder
+
+const todoRouter = express.Router()
+
+// GET all tasks
+todoRouter.get("/", async (req, res) => {
+    try {
+        const result = await query('select * from task')
+        const rows = result.rows ? result.rows : []
+        res.status(200).json(rows)
+    } catch (error) {
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
+})
+
+// POST new task
+todoRouter.post("/new", async (req, res) => {
+    try {
+        const result = await query('insert into task (description) values ($1) returning *', 
+        [req.body.description])
+        res.status(200).json({id: result.rows[0].id})
+    } catch (error) {
+        console.log(error)
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
+})
+
+// DELETE task
+todoRouter.delete("/delete/:id", async (req, res) => {
+    try {
+        const id = Number(req.params.id)
+        await query('delete from task where id = $1', [id])
+        res.status(200).json({id: id})
+    } catch (error) {
+        res.statusMessage = error
+        res.status(500).json({error: error})
+    }
+})
+
+// CRITICAL: Export the router
+module.exports = { todoRouter }
